5743346d052be5f21fd75666abafec83c0526329
Evi Dianasari <evidnsr@gmail.com>
2023-05-24 19:01:55 +0800

    Create index.php

---
+++ pages/about/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+        <title>toko baju eduwork</title>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
+        <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap4.min.css">
+        <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap4.min.js"></script>
+        <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script> -->
+        <link rel="stylesheet" href="styles\global.css">
+        
+        
+    </head>
+<body>
+
+  <!-- Include navbar -->
+  
+  <?php include 'pages\navbar\navbar.php'; ?>
+
+  <!-- Your page content -->
+  <?php include 'pages\about\about.php'; ?>
+
+  <!-- Include footer -->
+  <?php include 'pages\footer\footer.php'; ?>
+
+</body>
+</html>
