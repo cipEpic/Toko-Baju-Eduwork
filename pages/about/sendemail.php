3440b04d1f5421ca28a080d0c3503a4ae6e98751
Evi Dianasari <evidnsr@gmail.com>
2023-05-26 15:10:05 +0800

    Create sendemail.php

---
+++ pages/about/sendemail.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST['Name'];
+  $email = $_POST['Email'];
+  $message = $_POST['Message'];
+
+  // Email configuration
+  $to = "tugas.cipe@gmail.com";  // Replace with your email address
+  $subject = "New Contact Form Submission";
+  $headers = "From: $name <$email>";
+
+  // Compose the email message
+  $email_message = "Name: $name\n";
+  $email_message .= "Email: $email\n";
+  $email_message .= "Message: $message\n";
+
+  // Send the email
+  if (mail($to, $subject, $email_message, $headers)) {
+    echo "Email sent successfully.";
+  } else {
+    echo "Error sending email.";
+  }
+}
+
+header("manajemen.php");
+?>
