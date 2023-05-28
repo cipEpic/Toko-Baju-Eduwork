99789b42fc75bb1db6eefca3d72147e9a52a5cc6
Evi Dianasari <evidnsr@gmail.com>
2023-05-28 14:23:32 +0800

    Create prosestambah-jquery.php

---
+++ prosestambah-jquery.php
@@ -0,0 +1,49 @@
+<?php
+
+    // $name = $_POST['name'];
+    // $Image = $_POST['Image'];
+    // $price = $_POST['price'];
+    // $status = $_POST['status'];
+    // $id_category = $_POST['id_category'];
+
+    // include_once("database/connect.php");
+
+
+    // $result = mysqli_query($conn, "INSERT INTO `products`(`name`, `Image`, `price`, `status`, `id_category`) VALUES ('$name','$Image','$price','$status','$id_category');");
+
+    // // echo "INSERT INTO `products`(`name`, `Image`, `price`, `status`, `id_category`) VALUES ('$name','$Image','$price','$status','$id_category');";
+    // // exit();
+
+    // header("location:manajemen.php");
+?>
+
+
+<?php
+include_once("database/connect.php");
+
+// Retrieve form data
+$name = $_POST['name'];
+$image = $_POST['Image'];
+$price = $_POST['price'];
+$status = $_POST['status'];
+$category = $_POST['id_category'];
+// Insert data into the 'products' table
+$insertProductQuery = "INSERT INTO products (name, Image, price, status, id_category) VALUES ('$name', '$image', '$price', '$status', '$category')";
+mysqli_query($conn, $insertProductQuery);
+
+// Get the newly inserted product ID
+$productID = mysqli_insert_id($conn);
+
+// Insert data into the 'product_size' table
+if (isset($_POST['sizes'])) {
+    $sizes = $_POST['sizes'];
+    foreach ($sizes as $size) {
+        $insertProductSizeQuery = "INSERT INTO product_size (id_product, size_id) VALUES ('$productID', '$size')";
+        mysqli_query($conn, $insertProductSizeQuery);
+    }
+}
+
+
+// Redirect back to the tambah-jquery.php page after successful insertion
+header("location:manajemen.php");
+?>
